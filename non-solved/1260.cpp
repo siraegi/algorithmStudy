a2469059fe8d6e90449810f4ca9916ae07c283db
siraegi <shihabbong11@gmail.com>
2019-11-03 13:05:25 +0900

    BOJ-DFS,BFS

--- non-solved/1260.cpp
+++ non-solved/1260.cpp
@@ -1,7 +1,7 @@
 //
 //  1260.cpp
 //  Algorithm
-//
+//  DFS, BFS
 //  Created by ShiHaYeon on 02/11/2019.
 //  Copyright © 2019 ShiHaYeon. All rights reserved.
 //
@@ -9,6 +9,7 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <algorithm>
 using namespace std;
 bool visit[1001];
 void dfs(vector<vector<int>>& g, int n){
@@ -37,12 +38,16 @@ void bfs(vector<vector<int>>& g, int n){
 int main(){
     int N, M, V;
     cin >> N >> M >> V;
-    vector<vector<int>> g(M+1);
+    vector<vector<int>> g(N+1);
     int x, y;
     for(int i=0;i<M;i++){
         cin >> x >> y;
         g[x].push_back(y);
         g[y].push_back(x);
+    }
+    for(int i=1;i<=N;i++){
+        if(g[i].size()!=0)
+            sort(g[i].begin(),g[i].end());
     }
     dfs(g,V);
     fill_n(visit,1001,false);
